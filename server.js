c3d5ed56ba8d7ce7eb3dc44b7d3bc3037969fd82
Emily Westerback <emilywesterback2@gmail.com>
2021-07-20 10:42:09 -0700

    about to deploy

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+//app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
